d00d22ab5b37ec4a007cfff80f83cacb6448dcd9
hudaazadk <86004436+hudaazadk@users.noreply.github.com>
2024-08-16 09:37:50 +0530

    cookies

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//cookies
+
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + ";" + expires + ";path=/";
+  }
+  // Example usage
+  setCookie("username", "huda", 7);
+  function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  // Example usage
+  const username = getCookie("username");
+  console.log(username); 
+  function deleteCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+  }
+
 //countdown
 
 var countDownDate = new Date("Aug 30, 2024 15:00:00").getTime();
